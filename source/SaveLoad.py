3c20101ae06ed9f8eb275f49fa575af10a3e15b0
Daniel Park <parkd7@rpi.edu>
2024-02-27 16:33:31 -0500

    Add Numbers of Rows and Columns of Circuit

--- source/SaveLoad.py
+++ source/SaveLoad.py
@@ -4,22 +4,27 @@ import ParseCircuit
 
 
 def saveCircuit(circuit):
-    instructions = ParseCircuit.parse_circuit(circuit.grid)
     time = datetime.now()
+    instructions = ParseCircuit.parse_circuit(circuit.grid)
+    numRows = len(circuit.grid)
+    numColumns = len(circuit.grid[0])
 
-    hashData = str(time) + str(instructions) # TO BE ADDED: RCSID + name + bool student
+    # TO BE ADDED: RCSID + name + bool student
+    hashData = str(time) + str(instructions) + str(numRows) + str(numColumns)
     hashObj = hashlib.sha256()
     hashObj.update(hashData.encode())
     hashHex = hashObj.hexdigest()
 
-    save = {}
-    save["hash"] = str(hashHex)
-    save['time'] = str(time)
-    save["inst"] = str(instructions)
+    saveData = {}
+    saveData["hash"] = str(hashHex)
+    saveData['time'] = str(time)
+    saveData['row'] = numRows
+    saveData['col'] = numColumns
+    saveData["inst"] = str(instructions)
 
-    jsonObj = json.dumps(save, indent=2)
-    with open("save.json", 'w') as outfile:
-        outfile.write(jsonObj)
+    jsonObj = json.dumps(saveData, indent=4)
+    with open("save.qc", 'w') as saveFile:
+        saveFile.write(jsonObj)
     
     print("Save successful.")
 
