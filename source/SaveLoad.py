ca8074b02498758a4084a34aa753e4848976f841
Daniel Park <parkd7@rpi.edu>
2024-02-28 14:25:36 -0500

    Update Save

--- source/SaveLoad.py
+++ source/SaveLoad.py
@@ -4,35 +4,51 @@ import ParseCircuit
 
 
 def saveCircuit(circuit):
-    time = datetime.now()
-    instructions = ParseCircuit.parse_circuit(circuit.grid)
+    time = str(datetime.now())
+    instructions = str(ParseCircuit.parse_circuit(circuit.grid))
     numRows = len(circuit.grid)
     numColumns = len(circuit.grid[0])
 
     # TO BE ADDED: RCSID + name + bool student
-    hashData = str(time) + str(instructions) + str(numRows) + str(numColumns)
+    hashData = time + instructions + str(numRows) + str(numColumns)
     hashObj = hashlib.sha256()
     hashObj.update(hashData.encode())
-    hashHex = hashObj.hexdigest()
+    hashHex = str(hashObj.hexdigest())
 
     saveData = {}
-    saveData["hash"] = str(hashHex)
-    saveData['time'] = str(time)
+    saveData["hash"] = hashHex
+    saveData['time'] = time
     saveData['row'] = numRows
     saveData['col'] = numColumns
-    saveData["inst"] = str(instructions)
+    saveData["inst"] = instructions
 
     dirName = "saves"
-    fileName = "save.qc"
-    jsonObj = json.dumps(saveData, indent=4)
     if not os.path.exists(dirName):
         os.makedirs(dirName)
-    filePath = os.path.join(dirName, fileName)  
+
+    fileName = "save.qc"
+    filePath = os.path.join(dirName, fileName) 
+
+    jsonObj = json.dumps(saveData, indent=4)
     with open(filePath, 'w') as saveFile:
         saveFile.write(jsonObj)
-    
-    print("Save successful.")
 
 
 def loadCircuit():
-    pass
+    filePath = os.path.join("saves", "save.qc")
+    if not os.path.exists(filePath):
+        raise RuntimeError("Save file does not exist.")
+
+    jsonString = ""
+    with open(filePath, 'r') as loadFile:
+        for line in loadFile:
+            jsonString += line
+    loadData = json.loads(jsonString)
+
+    numRows = loadData['row']
+    numColumns = loadData['col']
+    instructionsString = loadData['inst']
+
+    # instructionString -> list of dict (instr)
+
+    return ParseCircuit.parse_instructions(numRows, numColumns, None)
